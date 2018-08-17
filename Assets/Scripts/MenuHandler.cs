ff69bf9bbecbc4a47f70f7df9844bf650a74eb12
Memia <memia@hotmail.com.au>
2018-08-17 16:32:38 +1000

    Week04
    We learnt how to change resolution, brightness and ambientlight brightness.

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -10,16 +10,23 @@ public class MenuHandler : MonoBehaviour
     #region Variables
     public GameObject mainMenu, optionsMenu, creditMenu;
     public bool showOptions;
-   //public bool showCredits;
-    public Slider volSlider, brightSlider;
+    //public bool showCredits;
+    public Slider volSlider, brightSlider, ambSlider;
     public AudioSource mainAudio;
     public Light dirLight;
+    public Vector2[] res = new Vector2[8];
+    public int resIndex;
+    public bool isFullScreen;
+    public Dropdown resDropdown;
     #endregion
     private void Start()
     {
-   
+
         mainAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
-    
+        dirLight = GameObject.FindGameObjectWithTag("DirLight").GetComponent<Light>();
+
+
+
     }
     public void LoadGame()
     {
@@ -36,7 +43,7 @@ public class MenuHandler : MonoBehaviour
     public void ToggleOptions()
     {
         OptionToggle();
-       // CreditToggle();
+        // CreditToggle();
     }
     /*bool CreditToggle()
     {
@@ -45,27 +52,33 @@ public class MenuHandler : MonoBehaviour
         mainMenu.SetActive(true);
         creditMenu.SetActive(false);
         return true;*/
-        
 
-        bool OptionToggle()
+
+    bool OptionToggle()
+    {
+        if (showOptions) //showOptions == true , or showOptions is true
         {
-            if (showOptions) //showOptions == true , or showOptions is true
-            {
-                showOptions = false;
-                mainMenu.SetActive(true);
-                optionsMenu.SetActive(false);
-                return false;
-            }
-            else
-            {
-                showOptions = true;
-                mainMenu.SetActive(false);
-                optionsMenu.SetActive(true);
+            showOptions = false;
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            return false;
+        }
+        else
+        {
+            showOptions = true;
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
             volSlider = GameObject.Find("Volume_Slider").GetComponent<Slider>();
             volSlider.value = mainAudio.volume;//slider dot starts where volume amount is, but won't affect the volume
+            brightSlider = GameObject.Find("Brightness_Slider").GetComponent<Slider>();
+            brightSlider.value = dirLight.intensity;
+            ambSlider = GameObject.Find("Ambience_Slider").GetComponent<Slider>();
+            ambSlider.value = RenderSettings.ambientIntensity;
+            resDropdown = GameObject.Find("Resolution").GetComponent<Dropdown>();
+            resDropdown.value = resIndex;
             return true;
-            }
         }
+    }
     public void Volume()
     {
         //volume is set to where slider is moved to
@@ -73,10 +86,21 @@ public class MenuHandler : MonoBehaviour
     }
     public void Brightness()
     {
-    }
-    }
+        dirLight.intensity = brightSlider.value;
 
-    
+    }
+    public void Ambience()
+    {
+        RenderSettings.ambientIntensity = ambSlider.value;
+    }
+    public void Resolution()
+    {
+        resIndex = resDropdown.value;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+    }
+}
+
+
 
 
 
