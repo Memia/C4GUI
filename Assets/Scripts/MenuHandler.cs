20f8210b9acf2b09ea49882a0ccd597223c8a414
Memia <memia@hotmail.com.au>
2018-08-24 16:02:41 +1000

    Week05
    In this lesson we learnt how to create save data and attempted to set key

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -8,23 +8,45 @@ using UnityEngine.EventSystems; //control the event (button things)
 public class MenuHandler : MonoBehaviour
 {
     #region Variables
-    public GameObject mainMenu, optionsMenu, creditMenu;
+    [Header("OPTIONS")]
     public bool showOptions;
-    //public bool showCredits;
-    public Slider volSlider, brightSlider, ambSlider;
+    [Header("Keys")]
+    public KeyCode holdingKey;
+    public KeyCode forward, backward, left, right, jump, crouch, sprint, interact;
+    [Header("References")]
     public AudioSource mainAudio;
+    public Slider volSlider, brightSlider, ambSlider;
     public Light dirLight;
     public Vector2[] res = new Vector2[8];
     public int resIndex;
     public bool isFullScreen;
     public Dropdown resDropdown;
+    [Header("Leybind References")]
+    public Text forwardText;
+    public Text backwardText, leftText, rightText, jumpText, crouchText, sprintText, interactText;
+    public GameObject mainMenu, optionsMenu, creditMenu;
+
+    //public bool showCredits;
+
+
     #endregion
     private void Start()
     {
 
         mainAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
         dirLight = GameObject.FindGameObjectWithTag("DirLight").GetComponent<Light>();
+        #region SetUp Keys
+        //set out keys to the preset keys we may have saved, else set the keys to default.
+        forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "W"));
+        backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "S"));
+        left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A"));
+        right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D"));
+        jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space"));
+        crouch = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "LeftControl"));
+        sprint = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift"));
+        interact = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E"));
 
+        #endregion
 
 
     }
@@ -97,6 +119,45 @@ public class MenuHandler : MonoBehaviour
     {
         resIndex = resDropdown.value;
         Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
+    }
+    public void Save()
+    {                         //file name, data
+        PlayerPrefs.SetString("Forward", forward.ToString());
+        PlayerPrefs.SetString("Backward", backward.ToString());
+        PlayerPrefs.SetString("Left", left.ToString());
+        PlayerPrefs.SetString("Right", right.ToString());
+        PlayerPrefs.SetString("Jump", jump.ToString());
+        PlayerPrefs.SetString("Crouch", crouch.ToString());
+        PlayerPrefs.SetString("Sprint", sprint.ToString());
+        PlayerPrefs.SetString("Interact", interact.ToString());
+    }
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (forward == KeyCode.None)
+        {
+            Debug.Log("Keycode:" + e.keyCode);
+            if (!(e.keyCode == backward || e.keyCode == left || e.keyCode == right || e.keyCode == jump || e.keyCode == crouch || e.keyCode == sprint || e.keyCode == interact))
+            {
+                forward = e.keyCode;
+                holdingKey = KeyCode.None;
+                forwardText.text = forward.ToString();
+            }
+        }
+        else
+        {
+            forward = holdingKey;
+        }
+
+    }
+    public void Forward()
+    {
+        if (!(backward == KeyCode.None || left == KeyCode.None || right == KeyCode.None || jump == KeyCode.None || crouch == KeyCode.None || sprint == KeyCode.None || interact == KeyCode.None))
+        {
+            holdingKey = forward;
+            forward = KeyCode.None;
+            forwardText.text = forward.ToString();
+        }
     }
 }
 
