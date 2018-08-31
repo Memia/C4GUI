aa1a1fba06052df03ddb092e1c0cb4905e2f6dc9
Memia <memia@hotmail.com.au>
2018-08-31 16:07:57 +1000

    Week06
    In this class we've set up pause menu, need to work on it further

---
+++ Assets/Scripts/GameManageMent.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManageMent : MonoBehaviour
+{
+    public bool paused;
+    public GameObject pausedPanel;
+    public GameObject settingsPanel;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        pausedPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if we perss escape Key
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // if it is not paused, pause
+            // if it is paused already, and isn't showing the settings, continue
+            // if it is paused already and is showing settings, unshow settings, don't continue
+            if (!paused)
+            {
+                Pause();
+            }
+            if (paused)
+            {
+                if (settingsPanel.active)
+                {
+                    settingsPanel.SetActive (false);
+                }
+                else
+                {
+                    Continue();
+                }
+            }
+        }
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        paused = true;
+        pausedPanel.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pausedPanel.SetActive(false);
+
+
+    }
+}
--- Assets/Scripts/KeyBindScript.cs
+++ Assets/Scripts/KeyBindScript.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
 public class KeyBindScript : MonoBehaviour
 {
-    public Slider volSlider, brightSlider, ambSlider;
     private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    [Header("Keys")]
     public Text up, down, left, right, jump, crouch, sprint;
     private GameObject curKey;
     private Color32 normal = new Color32(39, 33, 33, 22);
     private Color32 selected = new Color32(33, 55, 77, 77);
-    // Use this for initialization
 
-    void Start()
+    [Header("OPTIONS")]
+    public bool showOptions;
+    [Header("References")]
+    public AudioSource mainAudio;
+    public Slider volSlider, brightSlider, ambSlider;
+    public Light dirLight;
+    public Vector2[] res = new Vector2[8];
+    public int resIndex;
+    public bool isFullScreen;
+    public Dropdown resDropdown;
+    [Header("Leybind References")]
+    public GameObject mainMenu, optionsMenu, creditMenu;
+
+
+    public void LoadGame()
+{
+    SceneManager.LoadScene(1);
+}
+public void ExitGame()
+{
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    Application.Quit();
+}
+void Start()
     {
+   // mainAudio = GameObject.Find("MainMusic").GetComponent<AudioSource>();
+    //dirLight = GameObject.Find("DirLight").GetComponent<Light>();
+      //  mainAudio.volume = PlayerPrefs.GetFloat("Volume", 1);
+
         keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode),PlayerPrefs.GetString("Up","W")));
         keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "S")));
         keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
@@ -32,9 +61,14 @@ public class KeyBindScript : MonoBehaviour
         crouch.text = keys["Crouch"].ToString();
         sprint.text = keys["Sprint"].ToString();
 
-        volSlider.value = PlayerPrefs.GetFloat("volume", 1);
-
+        Resolution[] resolutions = Screen.resolutions;
+        foreach (Resolution res in resolutions)
+        {
+            print(res.width + "x" + res.height);
+        }
+        Screen.SetResolution(resolutions[0].width, resolutions[0].height, true);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -70,12 +104,62 @@ public class KeyBindScript : MonoBehaviour
     }
     public void SaveKeys()
     {
+        PlayerPrefs.SetFloat("Volume", volSlider.value);
+
         foreach (var key in keys)
         {
             PlayerPrefs.SetString(key.Key, key.Value.ToString());
         }
         PlayerPrefs.Save();
-      //  curKey.GetComponent<Image>().color = selected;
-        PlayerPrefs.SetFloat("Volume", volSlider.value);
+     //  curKey.GetComponent<Image>().color = selected;
+    }
+    public void ToggleOptions()
+    {
+        OptionToggle();
+        // CreditToggle();
+    }
+    bool OptionToggle()
+    {
+        if (showOptions) //showOptions == true , or showOptions is true
+        {
+            showOptions = false;
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            return false;
+        }
+        else
+        {
+            showOptions = true;
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+            volSlider = GameObject.Find("Volume_Slider").GetComponent<Slider>();
+            volSlider.value = mainAudio.volume;//slider dot starts where volume amount is, but won't affect the volume
+           // brightSlider = GameObject.Find("Brightness_Slider").GetComponent<Slider>();
+           // brightSlider.value = dirLight.intensity;
+           // ambSlider = GameObject.Find("Ambience_Slider").GetComponent<Slider>();
+          //  ambSlider.value = RenderSettings.ambientIntensity;
+          //  resDropdown = GameObject.Find("Resolution").GetComponent<Dropdown>();
+          //  resDropdown.value = resIndex;
+            return true;
+        }
+    }
+    public void Volume()
+    {
+        //volume is set to where slider is moved to
+        mainAudio.volume = volSlider.value;
+    }
+    public void Brightness()
+    {
+        dirLight.intensity = brightSlider.value;
+
+    }
+    public void Ambience()
+    {
+        RenderSettings.ambientIntensity = ambSlider.value;
+    }
+    public void Resolution()
+    {
+        resIndex = resDropdown.value;
+        Screen.SetResolution((int)res[resIndex].x, (int)res[resIndex].y, isFullScreen);
     }
 }
